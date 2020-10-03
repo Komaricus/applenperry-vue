39a831dcbb4b2d0951041abd681202f708b6ba55
Komaricus <komarovshura@yandex.ru>
2020-10-03 23:59:32 +0300

    Adds cspNonce for Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import light from './theme'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  options: {
+    cspNonce: '2726c7f26c'
+  }
+})
 
 import ru from 'vuetify/lib/locale/ru'
 
