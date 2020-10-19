27f772c6c907bf7d2e9589a91eae0d19c25af4ef
Komaricus <komarovshura@yandex.ru>
2020-10-19 19:28:22 +0300

    Updates forms settings: adds URLs

--- src/components/AdminForm/FormsSettings/countries.js
+++ src/components/AdminForm/FormsSettings/countries.js
@@ -6,7 +6,23 @@ export default {
       label: 'Название страны',
       placeholder: 'Введите название',
       required: true,
-      counter: 100
+      counter: 100,
+      slugSource: true
+    },
+    {
+      id: 'url',
+      type: 'slug-field',
+      label: 'Ссылка',
+      placeholder: 'Укажите ссылку',
+      required: true,
+      counter: 100,
+      tooltip: 'URL для навигации внутри сайта. Допустимы английские буквы, цифры и дефисы',
+      rules: [
+        value => {
+          const pattern = /^[a-z0-9-]+$/
+          return pattern.test(value) || 'Неверная ссылка'
+        }
+      ]
     },
     {
       id: 'flag',
--- src/components/AdminForm/FormsSettings/products-types.js
+++ src/components/AdminForm/FormsSettings/products-types.js
@@ -7,7 +7,23 @@ export default {
       placeholder: 'Введите название',
       required: true,
       counter: 200,
-      tooltip: 'Название типа товара для отображения в карточке товара'
+      tooltip: 'Название типа товара для отображения в карточке товара',
+      slugSource: true
+    },
+    {
+      id: 'url',
+      type: 'slug-field',
+      label: 'Ссылка',
+      placeholder: 'Укажите ссылку',
+      required: true,
+      counter: 200,
+      tooltip: 'URL для навигации внутри сайта. Допустимы английские буквы, цифры и дефисы',
+      rules: [
+        value => {
+          const pattern = /^[a-z0-9-]+$/
+          return pattern.test(value) || 'Неверная ссылка'
+        }
+      ]
     }
   ],
   creationTitle: 'Создание нового типа товара',
