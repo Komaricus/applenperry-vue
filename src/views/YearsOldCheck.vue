bff98f0816f2d7f4b72914db888077e72a48e1d6
Komaricus <komarovshura@yandex.ru>
2020-10-30 01:14:39 +0300

    Updates YearsOldCheck

--- src/views/YearsOldCheck.vue
+++ src/views/YearsOldCheck.vue
@@ -1,54 +1,33 @@
 <template>
   <section id="yo">
-    <img src="@/assets/images/logo.png" alt="logo" />
+    <div class="image-container">
+      <img src="@/assets/images/logo.png" alt="logo" />
+    </div>
     <div class="text-center">
       <h1 class="heading-4 mt-5 font-weight-regular">Откройте природный вкус общения!</h1>
-      <p class="mt-3">Для того, чтобы пользоваться сайтом укажите дату своего рождения</p>
-      <div class="datepicker" :class="{ 'margin-bottom': valid }">
-        <v-menu
-          v-model="menu"
-          :close-on-content-click="false"
-          transition="scale-transition"
-          offset-y
-          :nudge-top="2"
-          top
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="formattedDate"
-              :disabled="!valid"
-              prepend-inner-icon="fa-calendar"
-              readonly
-              dense
-              outlined
-              v-bind="attrs"
-              v-on="on"
-              hide-details
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            no-title
-            color="primary"
-            v-model="date"
-            @input="menu = false"
-            :first-day-of-week="1"
-            :max="new Date().toISOString().substr(0, 10)"
-            min="1920-01-01"
-            ref="picker"
-          ></v-date-picker>
-        </v-menu>
-
-        <v-btn
-          v-ripple="false"
-          height="40"
-          color="primary"
-          :disabled="!valid"
-          class="ml-2"
-          @click="check"
-          >Подтвердить</v-btn
-        >
-      </div>
+      <p class="mt-3 text-h6 font-weight-bold">Вам уже исполнилось 18 лет?</p>
+      <v-btn
+        color="primary"
+        v-ripple="false"
+        @click="valid = false"
+        :disabled="!valid"
+        outlined
+        width="100"
+        class="ma-1"
+        >Нет</v-btn
+      >
+      <v-btn
+        color="primary"
+        v-ripple="false"
+        @click="confirmYo"
+        :disabled="!valid"
+        width="100"
+        class="ma-1"
+        >Да</v-btn
+      >
+      <p class="text-body-1 mt-3">
+        Информация, представленная на сайте предназначена для лиц, достигших 18-летнего возраста.
+      </p>
       <v-alert max-width="350" class="mt-5 mx-auto" v-show="!valid" dense outlined type="error">
         Вы ещё не достигли возраста 18 лет
       </v-alert>
@@ -57,45 +36,17 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
-
 export default {
   name: 'YearsOldCheck',
   data() {
     return {
-      date: new Date().toLocaleDateString('fr-CA'),
-      valid: true,
-      menu: false
+      valid: true
     }
   },
   methods: {
-    ...mapMutations(['checkYearsOld']),
-    dateChanged(date) {
-      this.date = date
-    },
-    check() {
-      const now = new Date()
-      const yo = now - new Date(this.date)
-
-      if (yo >= 1000 * 3600 * 24 * 365 * 18 + 1000 * 3600 * 24 * 4) {
-        sessionStorage.setItem('yo', 'true')
-        window.location.reload(true)
-      } else {
-        this.valid = false
-      }
-    },
-    save(date) {
-      this.$refs.menu.save(date)
-    }
-  },
-  computed: {
-    formattedDate() {
-      return new Date(this.date).toLocaleDateString('ru')
-    }
-  },
-  watch: {
-    menu(val) {
-      val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+    confirmYo() {
+      sessionStorage.setItem('yo', 'true')
+      window.location.reload(true)
     }
   }
 }
@@ -111,6 +62,8 @@ export default {
   justify-content: center;
   flex-direction: column;
   padding: 20px;
+  max-width: 600px;
+  margin: 0 auto;
 
   h1 {
     font-size: 32px;
@@ -120,10 +73,13 @@ export default {
     font-size: 16px;
   }
 
-  img {
-    max-height: 400px;
-    max-width: 400px;
-    width: 70%;
+  .image-container {
+    padding: 20px;
+
+    img {
+      max-height: 400px;
+      max-width: 100%;
+    }
   }
 
   .datepicker {
